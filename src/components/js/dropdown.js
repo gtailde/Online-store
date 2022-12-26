b1872cce0a24857e045b63ec23d31d09e6060ca8
Oleh Nepiuk <gtailde@gmail.com>
2022-12-27 02:45:45 +0800

    Refactor: html and css markup, added styles, added js files for input, dual sliders and custom menu selection (drop-box).

---
+++ src/components/js/dropdown.js
@@ -0,0 +1,24 @@
+const dropdownBtn = document.querySelector(".dropdown__button"),
+dropdownList = document.querySelector(".dropdown__list"),
+dropdownListItem = document.querySelectorAll(".dropdown__list-item"),
+dropdownContainer = document.querySelector(".info-for-users__dropdown-container");
+
+let typeSort = 'auto';
+
+dropdownContainer.addEventListener("click", el => {
+  
+  if(el.target.className === "dropdown__button"){
+    dropdownList.classList.toggle("drop");
+  } else if (el.target.className === "dropdown__list-item"){
+    
+    typeSort = el.target.textContent;
+    dropdownBtn.textContent = typeSort; 
+    dropdownList.classList.toggle("drop");
+    
+    dropdownListItem.forEach(e => {e.className.includes("active-sort") ? e.classList.remove('active-sort') : e});
+    el.target.classList.add("active-sort");
+  
+  } else if(el.target.className === "dropdown__list-item active-sort") dropdownList.classList.toggle("drop");
+ 
+
+});
