acb93dff99a3bc4e96b41b4e6638f8010dd07fdb
qwertytr1 <makhnach20040208@gmail.com>
2023-01-10 17:50:06 +0300

    feat

--- src/components/ts/cards.ts
+++ src/components/ts/cards.ts
@@ -1,15 +1,13 @@
-import { data, allCategories, allManufacturers } from "../src/data/data";
+import { data, allCategories, allManufacturers } from "../../data/data";
 const BlockProducts: HTMLElement | null = document.querySelector('.main__block-products');
-import { loadFilters } from "./loader";
-
-loadFilters()
-function CardProd() {
+function CardProd(): void {
     for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < data[i].length; j++) {
-            let titleOnMass = data[i][j].title;
+        let MassValue = data[i];
+        for (let j = 0, len = MassValue.length; j < len; j++) {
+            let titleOnMass = MassValue[j].title;
             const Card: HTMLElement = document.createElement('div');
             Card.className = 'products-card';
-            BlockProducts?.append(Card);
+            BlockProducts?.prepend(Card);
             /////////////////////////////
             const cardFlex: HTMLElement = document.createElement('div');
             cardFlex.className = 'products-card__flex';
@@ -26,19 +24,23 @@ function CardProd() {
             ////////////////////////////
             const discount: HTMLElement | String = document.createElement('div');
             discount.className = 'discount';
-            discount.innerHTML = `-${data[i][j].discountPercentage}`;
+            discount.innerHTML = `-${MassValue[j].discountPercentage}`;
             cardPrice.prepend(discount);
             /////////////////////////////   
 
             const price: HTMLElement | String = document.createElement('h3');
-            price.innerHTML = `${data[i][j].price}`;
+            price.innerHTML = `${MassValue[j].price}`;
             cardPrice.prepend(price);
 
             /////////////////////////////
             const rating: HTMLElement | String = document.createElement('div');
             rating.className = 'products-card__rating';
-            data[i][j].rating === 5 ? rating.innerHTML = `Rating: ${data[i][j].rating}/5` : rating.innerHTML = `Rating: ${data[i][j].rating}/5.0`;
-
+            if (MassValue[j].rating === 5) {
+                rating.innerHTML = `Rating: ${MassValue[j].rating}/5`
+            }
+            else {
+                rating.innerHTML = `Rating: ${MassValue[j].rating}/5.0`
+            }
             Card.prepend(rating);
             /////////////////////////////
             const manufacrure: HTMLElement = document.createElement("div");
@@ -46,7 +48,7 @@ function CardProd() {
             Card?.prepend(manufacrure);
             const manufacrureName: HTMLElement | String = document.createElement('p');
             manufacrureName.className = 'manufacturer';
-            manufacrureName.innerHTML = `Manufacrer: ${data[i][j].brand}`
+            manufacrureName.innerHTML = `Manufacrer: ${MassValue[j].brand}`
             manufacrure.prepend(manufacrureName);
             ////////////////////////////////////////
             const CardTitle: HTMLElement = document.createElement('div');
@@ -59,11 +61,13 @@ function CardProd() {
             ///////////////////////////
             const Img: HTMLImageElement | String = document.createElement('img');
             Img.className = 'products-card__img';
-            Img.src = `${data[i][j].img[0]}`
+            Img.src = `${MassValue[j].img[0]}`
             Card?.prepend(Img);
+
+
         }
     }
-    console.log(data)
 }
 CardProd();
 export { CardProd, BlockProducts };
+
